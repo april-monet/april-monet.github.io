e85e65d8b7d7b5b4d406976272b086728ec35e2d
April Cound on BC computer <april.cound@bellevuecollege.edu>
2019-10-28 12:19:14 -0700

    created javascript folder, file, and test page

---
+++ javascript/primitives.js
@@ -0,0 +1,20 @@
+const myString = 'Learning JavaScript in class';
+const myFirstNumericVariable = 3;
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+
+var oldStyle = "This is the way we used to do it (with vars).";
+let newChangeableStyle = "This allows you to declare a variable that can be changed."
+const constantVariable = "This variable cannot be changed. Except...";
+const quotable = "This is a string where someone says 'Hey, I think we\'ll need an escaped single quote.'"
+
+console.log(myString);
+console.log(typeof myString);
+console.log(myFirstNumericVariable);
+console.log(typeof myFirstNumericVariable);
+console.log("Value of myBoolean is " + myBoolean);
+console.log(typeof myBoolean);
+console.log('Type:', typeof myNull)
+console.log('Value of myNull is ' + myNull)
+console.log('Value of myUndefined is ' + myUndefined)
