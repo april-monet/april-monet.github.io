d4eb8e86a3622bf406d9c175da346ad97d9f612f
April Cound on BC computer <april.cound@bellevuecollege.edu>
2019-10-30 11:53:16 -0700

    updated recipe file, added image

---
+++ javascript-tests/branching.js
@@ -0,0 +1,17 @@
+let height = 5;
+
+if (height >= 5){
+    console.log('We have a big one')
+}
+else{
+    console.log('Less than five.')
+}
+
+height = 3;
+
+if (height >= 5){
+    console.log('We have a big one')
+}
+else{
+    console.log('We have a little one.')
+}
