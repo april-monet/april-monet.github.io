2f865d4d2518698f50a0dffb9de0ea8406bb76ea
April Cound on BC computer <april.cound@bellevuecollege.edu>
2019-11-20 12:15:26 -0800

    object choices now showing output of functions on html page

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,29 +1,56 @@
-const simpleObject = {
-    sayName: function(){
-        console.log("Simple Object");
+
+window.onload = () => {
+
+    
+    const simpleObject = {
+        firstName: "James",
+        lastName: "Watson",
+        sayName: function(){
+            console.log(this.firstName, + " " + this.lastName);
+            return(this.firstName + " " + this.lastName);
+        }
+    }
+    
+    simpleObject.dynamicMethod = function(){
+        console.log("Dynamic Method");
+        return("This is a dynamically generated method on simpleObject.");
+    }
+    
+    // constructor function: uses Pascal case
+    function FunctionObject() {
+        FunctionObject.prototype.sayName = function() {
+            console.log("The name of this function is FunctionObject, a constructor function.");
+            return("The name of this function is FunctionObject, a constructor function.");
+        }
+    }
+    
+    
+    class CustomClass {
+        sayName(){
+            console.log("Custom class");
+            return("Custom class");
+        }
+    }
+    
+    const functionObject = new FunctionObject(); 
+    const customClass = new CustomClass();
+    // simpleObject.dynamicMethod();
+
+    const objectLiteralButton = document.getElementById('simpleObjectAction');
+    const functionButton = document.getElementById('functionObjectAction');
+    const customClassButton = document.getElementById('customClassAction');
+    const dynamicMethodButton = document.getElementById('dynamicMethodAction');
+
+    objectLiteralButton.onclick = () => {
+        simpleObjectDisplay.textContent = simpleObject.sayName();
+    }
+    functionButton.onclick = () => {
+        functionObjectDisplay.textContent = functionObject.sayName();
+    }
+    customClassButton.onclick = () => {
+        customClassDisplay.textContent = customClass.sayName();
+    }   
+    dynamicMethodButton.onclick = () => {
+        dynamicMethodDisplay.textContent = simpleObject.dynamicMethod();
     }
 }
-
-simpleObject.dynamicMethod = function(){
-    console.log("Dynamic Method");
-}
-
-// constructor function: uses Pascal case
- function FunctionObject() {
-    FunctionObject.prototype.sayName = function() {
-        console.log("The name of this function is FunctionObject, a constructor function.");
-    }
-}
-
-class CustomClass {
-    sayName(){
-        console.log("Custom class");
-    }
-}
-
-simpleObject.sayName();
-simpleObject.dynamicMethod();
-const functionObject = new FunctionObject();
-functionObject.sayName();
-const customClass = new CustomClass();
-customClass.sayName();
