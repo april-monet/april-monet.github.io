f9ea7b9b978ff58e04de90e5761d3983cae309f8
april-monet <april.cound@bellevuecollege.edu>
2019-12-01 17:55:38 -0800

    fixed area of circle in function-input, added sorting function to js-arrays

--- javascript-tests/function-input.js
+++ javascript-tests/function-input.js
@@ -1,65 +1,67 @@
 const feetPerMile = 5280;
 
 window.onload = () => {
+  const feetToInchesAction = document.getElementById("feetToInchesAction");
 
-    const feetToInchesAction = 
-        document.getElementById('feetToInchesAction');
+  feetToInchesAction.onclick = () => {
+    const feetToInchesInput = document.getElementById("feetToInchesInput");
+    const feetToInchesDisplay = document.getElementById("feetToInchesDisplay");
 
-    feetToInchesAction.onclick = () => {
-        const feetToInchesInput = document.getElementById('feetToInchesInput');
-        const feetToInchesDisplay = document.getElementById('feetToInchesDisplay');
+    feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
+  };
 
-        feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
-    }
+  const milesToFeetAction = document.getElementById("milesToFeetAction");
 
-    const milesToFeetAction = 
-    document.getElementById('milesToFeetAction');
+  milesToFeetAction.onclick = () => {
+    const milesToFeetInput = document.getElementById("milesToFeetInput");
+    const milesToFeetDisplay = document.getElementById("milesToFeetDisplay");
 
-    milesToFeetAction.onclick = () => {
-        const milesToFeetInput = document.getElementById('milesToFeetInput');
-        const milesToFeetDisplay = document.getElementById('milesToFeetDisplay');
+    milesToFeetDisplay.textContent = milesToFeet(milesToFeetInput.value);
+  };
 
-        milesToFeetDisplay.textContent = milesToFeet(milesToFeetInput.value);
-    }
-    
-    const areaOfTriangleAction = 
-    document.getElementById('areaOfTriangleAction');
+  const areaOfTriangleAction = document.getElementById("areaOfTriangleAction");
 
-    areaOfTriangleAction.onclick = () => {
-        const areaOfTriangleInputBase = document.getElementById('areaOfTriangleInputBase');
-        const areaOfTriangleInputHeight = document.getElementById('areaOfTriangleInputHeight');
-        const areaOfTriangleDisplay = document.getElementById('areaOfTriangleDisplay');
+  areaOfTriangleAction.onclick = () => {
+    const areaOfTriangleInputBase = document.getElementById(
+      "areaOfTriangleInputBase"
+    );
+    const areaOfTriangleInputHeight = document.getElementById(
+      "areaOfTriangleInputHeight"
+    );
+    const areaOfTriangleDisplay = document.getElementById(
+      "areaOfTriangleDisplay"
+    );
 
-        areaOfTriangleDisplay.textContent = areaOfTriangle(areaOfTriangleInputBase.value, areaOfTriangleInputHeight.value);
-    }
+    areaOfTriangleDisplay.textContent = areaOfTriangle(
+      areaOfTriangleInputBase.value,
+      areaOfTriangleInputHeight.value
+    );
+  };
 
-    const areaOfCircleAction = 
-    document.getElementById('areaOfCircleAction');
+  const areaOfCircleAction = document.getElementById("areaOfCircleAction");
 
-    areaOfCircleAction.onclick = () => {
-        const areaOfCircleInput = document.getElementById('areaOfCircleInput');
-        const areaOfCircleDisplay = document.getElementById('areaOfCircleDisplay');
+  areaOfCircleAction.onclick = () => {
+    const areaOfCircleInput = document.getElementById("areaOfCircleInput");
+    const areaOfCircleDisplay = document.getElementById("areaOfCircleDisplay");
 
-        areaOfCircleDisplay.textContent = areaOfCircle(areaOfCircleInput.value);
-    }
-    
-}
+    areaOfCircleDisplay.textContent = areaOfCircle(areaOfCircleInput.value);
+  };
+};
 
-const feetToInches = (feet) => {
-    return feet * 12;
-}
+const feetToInches = feet => {
+  return feet * 12;
+};
 
-const milesToFeet = (miles) => {
-    return miles * feetPerMile;
-}
+const milesToFeet = miles => {
+  return miles * feetPerMile;
+};
 
 const areaOfTriangle = (base, height) => {
-let area = (base * height) / 2;
-return area;
-}
+  let area = (base * height) / 2;
+  return area;
+};
 
-const areaOfCircle = (radius) => {
-    let piR = Math.PI * radius;
-    let area = piR * piR;
-    return area;
-}
+const areaOfCircle = radius => {
+  let area = Math.PI * radius * radius;
+  return area;
+};
--- javascript-tests/javascript-arrays.js
+++ javascript-tests/javascript-arrays.js
@@ -1,98 +1,116 @@
 const arrays = {
-    numbers: [
-        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5
-    ]
-}
+  numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5]
+};
 
 arrays.numbers.sort((a, b) => a - b);
 
 const techLanguages = {
-    languages: [
-        'JavaScript', 'HTML', 'CSS', 'Java', 'C#', 'Python', 'C/C++'
-    ]
-}
+  languages: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"]
+};
 
 techLanguages.languages.sort();
 
 const languageRanking = [
-    {
-        language: ' JavaScript',
-        rank: 1
-    },
-    {
-        language: ' HTML',
-        rank: 2
-    },
-    {
-        language: ' CSS',
-        rank: 3
-    },
-    {
-        language: ' SQL',
-        rank: 4
-    },
-    {
-        language: ' Java',
-        rank: 5
-    },
-    {
-        language: ' Bash/Shell',
-        rank: 6
-    },
-    {
-        language: ' Python',
-        rank: 7
-    }
-]
+  {
+    language: " HTML",
+    rank: 2
+  },
+  {
+    language: " Bash/Shell",
+    rank: 6
+  },
+  {
+    language: " SQL",
+    rank: 4
+  },
+  {
+    language: " JavaScript",
+    rank: 1
+  },
+  {
+    language: " CSS",
+    rank: 3
+  },
+  {
+    language: " Python",
+    rank: 7
+  },
+  {
+    language: " Java",
+    rank: 5
+  }
+];
+
+languageRanking.sort(compareRank);
+// Code omitted here
+
+function compareRank(a, b) {
+  const rankA = a.rank;
+  const rankB = b.rank;
+
+  let comparison = 0;
+  if (rankA > rankB) {
+    comparison = 1;
+  } else if (rankA < rankB) {
+    comparison = -1;
+  }
+  return comparison;
+}
 
 window.onload = () => {
-    const numbersDisplay = document.getElementById("numbers-display");
-    const listNumbers = document.getElementById("numbers-action");
+  const numbersDisplay = document.getElementById("numbers-display");
+  const listNumbers = document.getElementById("numbers-action");
 
-    const languagesDisplay = document.getElementById("language-names-display");
-    const listLanguages = document.getElementById("language-names-action");
+  const languagesDisplay = document.getElementById("language-names-display");
+  const listLanguages = document.getElementById("language-names-action");
 
-    const popularityDisplay = document.getElementById("language-popularity-display");
-    const listPopularity = document.getElementById("language-popularity-action");
+  const popularityDisplay = document.getElementById(
+    "language-popularity-display"
+  );
+  const listPopularity = document.getElementById("language-popularity-action");
 
-    const addToList = {
-        appendToList: (list, name) => {
-          const li = document.createElement("li");
-          li.appendChild(document.createTextNode(name));
-          list.appendChild(li);
-        }
-      };
+  const addToList = {
+    appendToList: (list, name) => {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(name));
+      list.appendChild(li);
+    }
+  };
 
-      const addToList2 = {
-        appendToList: (list, name1, name2) => {
-          const li = document.createElement("li");
-          li.appendChild(document.createTextNode(name1 ));
-          li.appendChild(document.createTextNode( name2));
-          list.appendChild(li);
-        }
-      };
+  const addToList2 = {
+    appendToList: (list, name1, name2) => {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(name1));
+      li.appendChild(document.createTextNode(name2));
+      list.appendChild(li);
+    }
+  };
 
-      listNumbers.onclick = () => {
-        const newList = document.getElementById("numbers-display");
-        for(number of arrays.numbers){
-            console.log(number);
-            addToList.appendToList(newList, number);
-        }
-      };
+  listNumbers.onclick = () => {
+    const newList = document.getElementById("numbers-display");
+    for (number of arrays.numbers) {
+      console.log(number);
+      addToList.appendToList(newList, number);
+    }
+  };
 
-      listLanguages.onclick = () => {
-        const newList = document.getElementById("language-names-display");
-        for(name of techLanguages.languages){
-            console.log(name);
-            addToList.appendToList(newList, name);
-        }
-      }
+  listLanguages.onclick = () => {
+    const newList = document.getElementById("language-names-display");
+    for (name of techLanguages.languages) {
+      console.log(name);
+      addToList.appendToList(newList, name);
+    }
+  };
 
-      listPopularity.onclick = () => {
-        const newList = document.getElementById("language-popularity-display");
-        for(name in languageRanking){
-            console.log(languageRanking[name].rank, languageRanking[name].language);
-            addToList2.appendToList(newList, languageRanking[name].rank, languageRanking[name].language);
-        }
-      }
-}
+  listPopularity.onclick = () => {
+    const newList = document.getElementById("language-popularity-display");
+    for (name in languageRanking) {
+      console.log(languageRanking[name].rank, languageRanking[name].language);
+      addToList2.appendToList(
+        newList,
+        languageRanking[name].rank,
+        languageRanking[name].language
+      );
+    }
+  };
+};
